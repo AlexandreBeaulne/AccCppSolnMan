c9c19e9ef45d77b8216d052e0ee929da55deb06e
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2012-09-22 17:44:44 +0800

    Chapter 1: Dealing with strings exercises

---
+++ Chap1/ex_1_2.cpp
@@ -0,0 +1,11 @@
+
+// ask for a person's name, and generate a framed greeting
+#include <iostream>
+#include <string>
+int main()
+{
+    const std::string exclam = "!";
+//    const std::string message = "Hello" + ", world" + exclam;
+//    std::cout << message << std::endl;
+    std::cout << "Cant use operand + on char arrays" << std::endl;
+}
