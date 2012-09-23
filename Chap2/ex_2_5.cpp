5f96f6e1c3362a90bd0690803602cc3db3e369bc
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2012-09-23 15:39:01 +0800

    Finished Chap 2 exercises

--- Chap2/ex_2_5.cpp
+++ Chap2/ex_2_5.cpp
@@ -1,5 +1,6 @@
 
 #include<iostream>
+#include<string>
 
 void rectangle(const int &y, const int &x){
     std::cout << std::endl;
@@ -13,6 +14,24 @@ void rectangle(const int &y, const int &x){
     std::cout << std::endl;
 }
 
+void isotriangle(const int &sidesize){
+
+    std::cout << std::endl;
+
+    std::string line(sidesize,' ');
+
+    for(int i=0;i<sidesize-1;++i){
+        line[0] = '*';
+        line[i] = '*';
+        std::cout << line << std::endl;
+        line[i] = ' ';
+    }
+
+    std::cout << std::string(sidesize,'*') << std::endl;
+
+    std::cout << std::endl;
+}
+
 void square(const int & size){
     rectangle(size,size);
 }
@@ -20,7 +39,7 @@ void square(const int & size){
 int main(){
     square(5);
     rectangle(4,6);
+    isotriangle(5);
     return 0;
 }
-
 
