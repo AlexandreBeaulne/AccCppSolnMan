115df8988eec27c5020afc3ecafd856b88d49704
AlexandreBeaulne <alexandre.beaulne@gmail.com>
2012-12-31 00:52:25 -0500

    Working on Chapter 6

---
+++ Chap6/ex_6_0_d.cpp
@@ -0,0 +1,28 @@
+
+#include<vector>
+#include<algorithm>
+
+#include "../Chap4/Student_info.h"
+
+bool pgrade(const Student_info& s) {
+    return !fgrade(s);
+}
+
+//// Double pass version
+//std::vector<Student_info> extract_fails(std::vector<Student_info>& students) {
+//    std::vector<Student_info> fail;
+//    remove_copy_if(students.begin(), students.end(),
+//        back_inserter(fail), pgrade);
+//    students.erase(remove_if(students.begin(), students.end(),
+//        fgrade), students.end());
+//    return fail;
+//}
+
+// Single pass version
+std::vector<Student_info> extract_fails(std::vector<Student_info>& students) {
+    std::vector<Student_info>::iterator iter =
+        stable_partition(students.begin(), students.end(), pgrade);
+    std::vector<Student_info> fail(iter, students.end());
+    students.erase(iter, students.end());
+    return fail;
+}
